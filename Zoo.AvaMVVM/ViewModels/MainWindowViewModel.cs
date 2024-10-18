a88622202040bad1c062fc0703ad39d646340db4
Enki <cell.enohp.apps@gmail.com>
2024-10-18 12:56:44 +0200

    Add MVVM

---
+++ Zoo.AvaMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Zoo.AvaMVVM.ViewModels
+{
+  public partial class MainWindowViewModel : ViewModelBase
+  {
+#pragma warning disable CA1822 // Mark members as static
+    private string _greeting =
+        "Welcome to Avalonia!";
+    public string Greeting { get => _greeting; set { _greeting = value; } }
+#pragma warning restore CA1822 // Mark members as static
+
+    public void ButtonAction()
+    {
+      Greeting = "Hallo";
+    }
+  }
+}
---
+++ Zoo.AvaMVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Zoo.AvaMVVM.ViewModels
+{
+  public class ViewModelBase : ObservableObject
+  {
+  }
+}
